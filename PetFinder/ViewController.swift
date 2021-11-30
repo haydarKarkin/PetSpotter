fffcc31fddc7ba112e90d2b01516405f19d9a87c
Haydar Karkin <haydar.karkin@gmail.com>
2021-11-30 20:25:28 +0300

    Project init

---
+++ PetFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetFinder
+//
+//  Created by hkarkin on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PetFinderUITests/PetFinderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PetFinderUITestsLaunchTests.swift
+//  PetFinderUITests
+//
+//  Created by hkarkin on 30.11.2021.
+//
+
+import XCTest
+
+class PetFinderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
