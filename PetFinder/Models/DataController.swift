1a8ebb773f6d190a81d6336b0e8637646d6c9801
Haydar Karkin <haydar.karkin@gmail.com>
2021-11-30 20:31:55 +0300

    Add DataController for managing core data

---
+++ PetFinder/Models/DataController.swift
@@ -0,0 +1,52 @@
+//
+//  DataController.swift
+//  PetFinder
+//
+//  Created by hkarkin on 30.11.2021.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func configureContexts() {
+        viewContext.automaticallyMergesChangesFromParent = true
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+    }
+    
+    func load(completion : (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self.autoSaveViewContext()
+            self.configureContexts()
+            completion?()
+        }
+    }
+}
+
+// MARK: - Autosaving
+
+extension DataController {
+    // This function will save each 30 seconds if the context has changes
+    func autoSaveViewContext(interval: TimeInterval = 30) {
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+}
