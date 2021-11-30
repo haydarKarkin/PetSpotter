bbfb8764e10e5253253060d769133ff54795b991
Haydar Karkin <haydar.karkin@gmail.com>
2021-11-30 21:03:56 +0300

    Add configuration for the project

---
+++ PetFinder/Shared/Coordinator/Coordinator.swift
@@ -0,0 +1,19 @@
+//
+//  Coordinator.swift
+//  PetFinder
+//
+//  Created by hkarkin on 30.11.2021.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+    func coordinate(to coordinator: Coordinator)
+}
+
+extension Coordinator {
+    func coordinate(to coordinator: Coordinator) {
+        coordinator.start()
+    }
+}
---
+++ PetFinder/Shared/Environment/Environment.swift
@@ -0,0 +1,52 @@
+//
+//  Environment.swift
+//  PetFinder
+//
+//  Created by hkarkin on 30.11.2021.
+//
+
+import Foundation
+
+enum EnvType: String {
+    case debug = "DEBUG"
+    case release = "RELEASE"
+}
+
+public enum Environment {
+    // MARK: - Keys
+    enum Keys {
+        enum Plist {
+            static let rootURL = "ROOT_URL"
+            static let envName = "ENV_TYPE"
+        }
+    }
+    
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+    
+    // MARK: - Plist values
+    static let rootURL: URL = {
+        guard let rootURLString = Environment.infoDictionary[Keys.Plist.rootURL] as? String else {
+            fatalError("Root URL not set in plist for this environment")
+        }
+        guard let url = URL(string: rootURLString) else {
+            fatalError("Root URL is invalid")
+        }
+        return url
+    }()
+    
+    static let env: EnvType = {
+        guard let envString = Environment.infoDictionary[Keys.Plist.envName] as? String else {
+            fatalError("Environment type not set in plist for this environment")
+        }
+        guard let envType = EnvType.init(rawValue: envString) else {
+            fatalError("Environment type is invalid")
+        }
+        return envType
+    }()
+}
