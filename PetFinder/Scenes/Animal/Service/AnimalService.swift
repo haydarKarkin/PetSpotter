f909c16fc3c344c3858849e5e1c5ed7e7b4da3ad
Haydar Karkin <haydar@socio.events>
2021-12-11 00:20:19 +0300

    Add getAnimals feature for getting animals response

--- PetFinder/Scenes/Animal/Service/AnimalService.swift
+++ PetFinder/Scenes/Animal/Service/AnimalService.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 protocol AnimalServiceType {
+    func getAnimals(page: Int, completion: @escaping(Result<Animals, Error>) -> ())
 }
 
 class AnimalService: AnimalServiceType {
@@ -16,5 +17,20 @@ class AnimalService: AnimalServiceType {
     
     init(provider: ClientProvider<AnimalAPI>) {
         self.provider = provider
+    }
+    
+    func getAnimals(page: Int, completion: @escaping(Result<Animals, Error>) -> ()) {
+        provider.request(target: .animals(perPage: Configs.Network.paginationNumber,
+                                          page: page),
+                         responseType: Animals.self) { result in
+            
+            switch result {
+            case .success(let resp):
+                completion(.success(resp))
+            case .failure(let error):
+                completion(.failure(error))
+                print(error.localizedDescription)
+            }
+        }
     }
 }
