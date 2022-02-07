393a23e785a3cb324c7e958b123b44139012eebd
Haydar Karkin <haydar.karkin@gmail.com>
2022-02-07 21:34:06 +0300

    Fix alignment issue for onboarding scene

--- PetFinder/Scenes/Onboarding/Features/Onboarding/OnboardingVC.swift
+++ PetFinder/Scenes/Onboarding/Features/Onboarding/OnboardingVC.swift
@@ -13,6 +13,16 @@ class OnboardingVC: ViewController<OnboardingVM> {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func makeUI() {
         super.makeUI()
     }
